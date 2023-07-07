a5628c1e6e1960f81d29ee6f770f3704726543b1
chorochev <chorochev@ya.ru>
2023-07-07 14:05:19 +0600

    function

--- part02/functions.ts
+++ part02/functions.ts
@@ -15,7 +15,16 @@ function logBrtMsg2(isBirthday: boolean, age: number, userName: string): void {
   return undefined;
 }
 
+function getBrtMsg(isBirthday: boolean, age: number, userName: string): string {
+  if (isBirthday) {
+    return `Congrats ${userName.toUpperCase()}, age ${age + 1}`;
+  } else {
+    return "Error";
+  }
+}
+
 logBrtMsg(isBirthdayData, ageDate, userNameData);
 // ts-node .\functions.ts
 // logBrtMsg("yes", ageDate, userNameData); // error TS2345: Argument of type 'string' is not assignable to parameter of type 'boolean'.
 logBrtMsg2(isBirthdayData, ageDate, userNameData);
+console.log(getBrtMsg(isBirthdayData, ageDate, userNameData));
