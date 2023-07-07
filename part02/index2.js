93086bbcecfefdbfb3276c4752fe64b038207ce0
chorochev <chorochev@ya.ru>
2023-07-07 13:25:19 +0600

    example

--- part02/index2.js
+++ part02/index2.js
@@ -1,6 +1,7 @@
-var isBirthday = 1;
+var isBirthday = true;
 // const age = 40;
-var age = "40";
+// const age = "40";
+var age = 40;
 var userSurname = "John";
 if (isBirthday) {
     console.log("Congrats ".concat(userSurname, ", age ").concat(age + 1));
--- part02/index2.ts
+++ part02/index2.ts
@@ -1,7 +1,8 @@
-const isBirthday = 1;
+const isBirthday: boolean = true;
 // const age = 40;
-const age = "40";
-const userSurname = "John";
+// const age = "40";
+const age: number = 40;
+const userSurname: string = "John";
 
 if (isBirthday) {
   console.log(`Congrats ${userSurname}, age ${age + 1}`);
