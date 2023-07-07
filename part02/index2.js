6ac7501623bd74fa4aefe587d23c492e7cd1c5f7
chorochev <chorochev@ya.ru>
2023-07-07 13:14:38 +0600

    example

---
+++ part02/index2.js
@@ -0,0 +1,6 @@
+var isBirthday = 1;
+var age = 40;
+var userSurname = "John";
+if (isBirthday) {
+    console.log("Congrats ".concat(userSurname, ", age ").concat(age + 1));
+}
---
+++ part02/index2.ts
@@ -0,0 +1,7 @@
+const isBirthday = 1;
+const age = 40;
+const userSurname = "John";
+
+if (isBirthday) {
+  console.log(`Congrats ${userSurname}, age ${age + 1}`);
+}
