0a40b58c93b4b4de156d54e651ba491fe20883fc
chorochev <chorochev@ya.ru>
2023-07-07 15:52:44 +0600

    examples

--- part02/type_never.ts
+++ part02/type_never.ts
@@ -16,3 +16,6 @@ function getBrtMsg3(
 
 console.log(getBrtMsg3(true, 40, "John"));
 // console.log(getBrtMsg3(false, 43, "Sam")); // throw new Error(msg);
+
+// const smth : never = undefined; // Type 'undefined' is not assignable to type 'never'.ts(2322)
+// const smth: never = null; // Type 'null' is not assignable to type 'never'.ts(2322)
