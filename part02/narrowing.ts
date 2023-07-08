0e9d7759eac7a72a525cd94b6ba683620916a470
chorochev <chorochev@ya.ru>
2023-07-08 19:47:53 +0600

    narrowing

--- part02/narrowing.ts
+++ part02/narrowing.ts
@@ -9,3 +9,19 @@ function narrowing_PrintMsg(msg: string | number): void {
 
 narrowing_PrintMsg(13);
 narrowing_PrintMsg("A test message.");
+
+console.log("************************");
+
+function narrowing_PrintMsg2(msg: string[] | number | boolean): void {
+  if (Array.isArray(msg)) {
+    msg.forEach((m) => console.log(m)); // (parameter) msg: string[]
+  } else if (typeof msg === "number") {
+    console.log(msg.toFixed()); // (parameter) msg: number
+  } else {
+    console.log(msg); // (parameter) msg: boolean
+  }
+}
+
+narrowing_PrintMsg2(["msg1", "msg2", "msg3"]);
+narrowing_PrintMsg2(13);
+narrowing_PrintMsg2(true);
