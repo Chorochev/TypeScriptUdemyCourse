151e73aede2dd8d5d682d3b772c4c26d3e71ccd4
chorochev <chorochev@ya.ru>
2023-07-09 11:46:51 +0600

    equality

---
+++ javascript/operators.js
@@ -0,0 +1,12 @@
+console.log("Equality (==):");
+console.log(1 == 1); // true
+console.log('hello' == 'hello'); // true
+console.log('1' ==  1); // true
+console.log(0 == false); // true
+
+console.log("Strict equality (===)");
+console.log(1 === 1); // true
+console.log("hello" === "hello"); // true
+console.log("1" === 1); // false
+console.log(0 === false); // false
+
