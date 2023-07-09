dda5c6b78a4e6586940dc982f1ae2caca60a8ddc
chorochev <chorochev@ya.ru>
2023-07-09 13:18:57 +0600

    Object literal type

---
+++ part02/objects_literals.ts
@@ -0,0 +1,31 @@
+const serverConfig31 = {
+  protocol: "https",
+  port: 3001,
+};
+
+function startServer31(
+  protocol: "http" | "https",
+  port: 3000 | 3001
+): "Server start" {
+  console.log(`Server started on ${protocol}://server:${port}`);
+  return "Server start";
+}
+
+// Argument of type 'string' is not assignable to parameter of type '"https" | "http"'.ts(2345)
+// startServer31(serverConfig31.protocol, serverConfig31.port); // Error
+
+// Object literal type
+const serverConfig32: { protocol: "http" | "https"; port: 3000 | 3001 } = {
+  protocol: "https",
+  port: 3001,
+};
+
+function startServer32(
+  protocol: "http" | "https",
+  port: 3000 | 3001
+): "Server start" {
+  console.log(`Server started on ${protocol}://server:${port}`);
+  return "Server start";
+}
+
+startServer32(serverConfig32.protocol, serverConfig32.port); // Ok
