e01e63eac4fa02685095d4d08d1cd34e05858aa4
chorochev <chorochev@ya.ru>
2023-07-10 16:36:22 +0600

    Non-null

--- part03/Non_Null_Undefined.ts
+++ part03/Non_Null_Undefined.ts
@@ -17,9 +17,13 @@
     age: 50,
   };
 
-  const dbName = "mydb";
+  let dbName: string;
+  sendUserData(user, "mydb");
+  // console.log(dbName); // Variable 'dbName' is used before being assigned.ts(2454)
+  console.log(dbName!); // Ok
 
   function sendUserData(obj: User, db?: string): void {
+    dbName = "dijfhjdh";
     // obj.parents! - it always exists
     console.log(obj.parents!.father?.toLocaleUpperCase(), db?.toLowerCase());
   }
