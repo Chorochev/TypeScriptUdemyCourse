c83241754beb6ef145bdbb02b442240e6d28fc41
chorochev <chorochev@ya.ru>
2023-07-10 16:20:26 +0600

    Optional Properties

--- part03/optional.ts
+++ part03/optional.ts
@@ -28,3 +28,29 @@
     addr: "tttttttt", // !!!
   };
 }
+
+{
+  interface User {
+    login: string;
+    password: string;
+    age: number;
+    addr?: string; // ? - Optional Properties
+    parents?: // ? - Optional Properties
+    {
+      mother?: string; // ? - Optional Properties
+      father?: string; // ? - Optional Properties
+    };
+  }
+
+  const user: User = {
+    login: "first!",
+    password: "123",
+    age: 50,
+  };
+
+  const dbName = "mydb";
+
+  function sendUserData(obj: User, db?: string): void {
+    console.log(obj, db?.toLowerCase());
+  }
+}
