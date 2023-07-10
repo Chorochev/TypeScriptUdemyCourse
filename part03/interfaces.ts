a13991615d4696635bc211f8c66ea93bc63d4d94
chorochev <chorochev@ya.ru>
2023-07-10 11:15:45 +0600

    interface extends

--- part03/interfaces.ts
+++ part03/interfaces.ts
@@ -8,3 +8,20 @@ const serverConfigI: IConfig = {
   protocol: "https",
   port: 3001,
 };
+
+// interface extends
+
+interface IRole {
+  role: string;
+}
+
+interface IConfigWithRole extends IConfig, IRole {
+  test: string;
+}
+
+const serverConfig2: IConfigWithRole = {
+  protocol: "https",
+  port: 3001,
+  role: "admin",
+  test: "Hello world",
+};
