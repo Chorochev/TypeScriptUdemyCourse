8d51eac2c7b47a14ce65b010d0133f5c6d55603c
chorochev <chorochev@ya.ru>
2023-07-10 16:12:13 +0600

    optional

--- part03/optional.ts
+++ part03/optional.ts
@@ -3,12 +3,28 @@
     login: string;
     password: string;
     age: number;
-    addr?: string;
+    addr?: string; // !!!
   }
 
   const user: User = {
     login: "first!",
     password: "123",
     age: 50,
+  };
+}
+
+{
+  interface User {
+    login: string;
+    password: string;
+    age: number;
+    addr: string | undefined; // !!!
+  }
+
+  const user: User = {
+    login: "first!",
+    password: "123",
+    age: 50,
+    addr: "tttttttt", // !!!
   };
 }
