e2831fc65614054e86dc1f8e9b805e1c990ff0f4
chorochev <chorochev@ya.ru>
2023-07-10 17:39:27 +0600

    enum

--- part03/enum.ts
+++ part03/enum.ts
@@ -10,3 +10,21 @@
   }
   frame("id", RIGHT, "fast!");
 }
+
+{
+  enum Directions {
+    TOP,
+    RIGHT,
+    LEFT,
+    BOTTOM,
+  }
+
+  function frame(elem: string, dir: Directions, tFunc: string): void {
+    if (dir === Directions.RIGHT) {
+      console.log(`elem=> ${elem}, dir=> ${dir}, tFunc=>${tFunc}`);
+    } else {
+      console.log("...");
+    }
+  }
+  frame("id", Directions.RIGHT, "fast!");
+}
