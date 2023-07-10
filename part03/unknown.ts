42d865fee76305522f5e742fe69f4825a5eecd71
chorochev <chorochev@ya.ru>
2023-07-10 20:11:49 +0600

    unknown

--- part03/unknown.ts
+++ part03/unknown.ts
@@ -5,3 +5,11 @@
   // Runtime error
   // data.find((e) => e); // TypeError: data.find is not a function
 }
+
+{
+  let smth: unknown;
+  smth = "str";
+  //let data: string[] = smth; // Type 'unknown' is not assignable to type 'string[]'.ts(2322)
+  let data: string[] = ["str1", "str2"]; // Ok
+  console.log(data.find((e) => e)); // Ok
+}
