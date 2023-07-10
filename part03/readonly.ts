2c1667d163a81e88557771ec3897bb5662365fec
chorochev <chorochev@ya.ru>
2023-07-10 16:50:57 +0600

    readonly

--- part03/readonly.ts
+++ part03/readonly.ts
@@ -22,4 +22,14 @@
   class Animal {
     readonly name: string = "cat";
   }
+
+  const basicPorts: readonly number[] = [3000, 3001, 3002];
+  // basicPorts[0] = 5; // Index signature in type 'readonly number[]' only permits reading.ts(2542)
+  // basicPorts.push(5); // Property 'push' does not exist on type 'readonly number[]'.ts(2339)
+}
+
+{
+  const basicPorts: readonly [number, ...string[]] = [3000, "3001", "3002"];
+  // basicPorts[0] = 5; // Cannot assign to '0' because it is a read-only property.ts(2540)
+  // basicPorts.push(5); // Property 'push' does not exist on type 'readonly [number, ...string[]]'.ts(2339)
 }
