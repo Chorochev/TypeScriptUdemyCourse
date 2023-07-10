27228c92b0f54728b367af2062cbf932256e323d
chorochev <chorochev@ya.ru>
2023-07-10 16:54:38 +0600

    Readonly

--- part03/readonly.ts
+++ part03/readonly.ts
@@ -33,3 +33,24 @@
   // basicPorts[0] = 5; // Cannot assign to '0' because it is a read-only property.ts(2540)
   // basicPorts.push(5); // Property 'push' does not exist on type 'readonly [number, ...string[]]'.ts(2339)
 }
+
+{
+  interface IUser {
+    readonly login: string;
+    password: string;
+    age: number;
+    addr?: string;
+    parents?: {
+      mother?: string;
+      father?: string;
+    };
+  }
+
+  const userFreeze: Readonly<IUser> = {
+    login: "first!",
+    password: "123",
+    age: 50,
+  };
+
+  // userFreeze.age = 77; // Cannot assign to 'age' because it is a read-only property.ts(2540)
+}
