7d8657cd9427adac7a74500a208d3d2bc3986739
chorochev <chorochev@ya.ru>
2023-07-10 17:44:08 +0600

    enum string

--- part03/enum.ts
+++ part03/enum.ts
@@ -34,3 +34,27 @@
   }
   frame("id", Directions.RIGHT, TimingFunc.LINEAR);
 }
+
+{
+  enum Directions {
+    TOP,
+    RIGHT,
+    LEFT,
+    BOTTOM,
+  }
+
+  enum TimingFunc {
+    EASE = "ease",
+    EASE_IN = "ease_in",
+    LINEAR = "linear",
+  }
+
+  function frame(elem: string, dir: Directions, tFunc: TimingFunc): void {
+    if (dir === Directions.RIGHT) {
+      console.log(`elem=> ${elem}, dir=> ${dir}, tFunc=> ${tFunc}`);
+    } else {
+      console.log("...");
+    }
+  }
+  frame("id", Directions.RIGHT, TimingFunc.LINEAR);
+}
