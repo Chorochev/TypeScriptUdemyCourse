5dcfa99606058a8566c25574ecb6be00d120282c
chorochev <chorochev@ya.ru>
2023-07-11 11:13:08 +0600

    assertion

--- part03/assertion.ts
+++ part03/assertion.ts
@@ -9,5 +9,10 @@
   };
 
   fetchData("qqq", "GET");
-  //   fetchData(reqOption.url, reqOption.method); // Argument of type 'string' is not assignable to parameter of type '"GET" | "POST"'.ts(2345)
+  // fetchData(reqOption.url, reqOption.method); // Argument of type 'string' is not assignable to parameter of type '"GET" | "POST"'.ts(2345)
+  const method = "GET";
+  fetchData(reqOption.url, method); // Ok
+
+  const methodStr: string = "GET";
+  // fetchData(reqOption.url, methodStr); // Argument of type 'string' is not assignable to parameter of type '"GET" | "POST"'.ts(2345)
 }
