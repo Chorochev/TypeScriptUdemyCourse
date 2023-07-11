89f67b1f8475be6da16f6d0d4f7af75b77a0d3ec
chorochev <chorochev@ya.ru>
2023-07-11 14:45:39 +0600

    type guard

---
+++ part03/type_guard2.ts
@@ -0,0 +1,19 @@
+{
+  function isNumber(n: string[] | number | boolean): n is number {
+    return typeof n === "number";
+  }
+
+  function printMsg(msg: string[] | number | boolean): void {
+    if (Array.isArray(msg)) {
+      msg.forEach((m) => console.log(m)); // (parameter) msg: string[]
+    } else if (isNumber(msg)) {
+      console.log(msg.toFixed()); // (parameter) msg: number
+    } else {
+      console.log(msg); // (parameter) msg: boolean
+    }
+  }
+
+  printMsg(4);
+  printMsg(true);
+  printMsg(["test1", "test2"]);
+}
