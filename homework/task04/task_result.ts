5114bd94213999fe6e4fc8da66350d226b9b6f50
chorochev <chorochev@ya.ru>
2023-07-11 09:41:45 +0600

    edit function

--- homework/task04/task_result.ts
+++ homework/task04/task_result.ts
@@ -26,24 +26,19 @@
     marks?: unknown;
   }
 
-  // function playMedia(
-  // 	{ name, type, format, subtitles, marks }: интерфейс = {
-  // 		name: "example",
-  // 		type: один из типов,
-  // 		format: один из форматов,
-  // 	}
-  // ): string {
-  // 	let marksLog;
-  //     // Создать функционал, что если marks - это массив, то "сложить" все эелементы в одну строку и поместить в marksLog
-  //     // Если это строка, то просто поместить её в marksLog
-  //     // Если что-то другое - то marksLog = "Unsupported type of marks"
-  //     // Не допускайте any!
-  // 	console.log(`Media ${name}${format} is ${type}
-  //     Marks: ${marksLog}
-  //     Subtitles: ${subtitles ?? "none"}`);
-  //     // помните что это за оператор ??
-  // 	return "Media started";
-  // }
+  function playMedia(data: IPlayMediaData): string {
+    let marksLog;
+    // Создать функционал, что если marks - это массив, то "сложить" все эелементы в одну строку и поместить в marksLog
+    // Если это строка, то просто поместить её в marksLog
+    // Если что-то другое - то marksLog = "Unsupported type of marks"
+    // Не допускайте any!
+    console.log(`Media ${data.name}${data.format} is ${data.type}
+      Marks: ${marksLog}
+      Subtitles: ${data.subtitles ?? "none"}`);
+    // помните что это за оператор ??
+    return "Media started";
+  }
+
   // playMedia({
   // 	name: "WoW",
   // 	format: один из форматов,
