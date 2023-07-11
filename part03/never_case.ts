6ae88d1d36710b7194a721d27b875d8f650d076e
chorochev <chorochev@ya.ru>
2023-07-11 16:00:02 +0600

    never

---
+++ part03/never_case.ts
@@ -0,0 +1,18 @@
+{
+  function func1(item: "true" | "false"): void {
+    if (item === "false") {
+      console.log("(parameter) item: false");
+      // const smth: never = item; // Error
+    } else if (item === "true") {
+      console.log("(parameter) item: true");
+      // const smth: never = item; // Error
+    } else {
+      console.log("(parameter) item: never");
+      const smth: never = item; // Ok
+    }
+  }
+
+  func1("true");
+  func1("false");
+  //   func1("something"); // Error
+}
