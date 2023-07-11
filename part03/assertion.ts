64a2f586699e743d0b6ad4ce8dfaff59e55d62b4
chorochev <chorochev@ya.ru>
2023-07-11 12:09:57 +0600

    assertions

--- part03/assertion.ts
+++ part03/assertion.ts
@@ -58,3 +58,20 @@
   fetchData(reqOption.url, reqOption.method as "GET"); // Ok
   fetchData(reqOption.url, <"GET">reqOption.method); // Ok
 }
+
+{
+  let a = "value" as const; // let a: "value"
+  let b = { f: 100 } as const; // let b: { readonly f: 100; }
+  let c = [] as const; //let c: readonly []
+
+  let value = "value";
+  let arr = ["sd", "dff"];
+  let obj = { f: 100 };
+  let T0 = value; // OK
+  // let T1 = value as const; // A 'const' assertions can only be applied to references to enum members, or string, number, boolean, array, or object literals.ts(1355)
+  let T2 = obj; // Ok
+  // let T3 = obj as const; // Error
+
+  let T5 = Math.round(Math.random() * 1) ? "yes" : "no"; // let T5: string
+  // let T6 = (Math.round(Math.random() * 1) ? "yes" : "no") as const; // Error
+}
