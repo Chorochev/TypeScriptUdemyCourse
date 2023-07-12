a5c2970bfed055ebdf0ba568c243684b7f5b80ee
chorochev <chorochev@ya.ru>
2023-07-12 13:15:30 +0600

    void

--- part03/void.ts
+++ part03/void.ts
@@ -14,4 +14,13 @@
 
   const n = retNum(); // const n: void
   console.log(n); // 5
+
+  const names = ["Anna", "John"]; // const names: string[]
+  const newArr = names.slice(); // const newArr: string[]
+
+  // !!! forEach: void
+  // (method) Array<string>.forEach(callbackfn: (value: string, index: number, array: string[]) => void, thisArg?: any): void
+  // (parameter) arr: string[]
+  // !!! (method) Array<string>.push(...items: string[]): number
+  names.forEach((name, i, arr) => arr.push("Hey!"));
 }
