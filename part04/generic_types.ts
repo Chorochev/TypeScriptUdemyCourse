10567a1b7bf27c918272040118e71d088b2bcdda
chorochev <chorochev@ya.ru>
2023-07-12 17:33:48 +0600

    Generic type

--- part04/generic_types.ts
+++ part04/generic_types.ts
@@ -4,3 +4,20 @@
   type Smth<T> = T;
   const num: Smth<number> = 5;
 }
+
+{
+  type User<T> = {
+    login: T;
+    age: number;
+  };
+
+  const user1: User<string> = {
+    login: "admin",
+    age: 55,
+  };
+
+  const user2: User<"str"> = {
+    login: "str",
+    age: 55,
+  };
+}
