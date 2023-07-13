31a88b913be30639f300a8a8036d17b838378e2d
chorochev <chorochev@ya.ru>
2023-07-13 13:31:22 +0600

    generic readonly

---
+++ part04/generic_build.ts
@@ -0,0 +1,21 @@
+// Readonly, Partial, Required, Pick, Record
+{
+  const arr1: Array<number> = [1, 2, 3]; // const arr1: number[]
+  const arr2: number[] = [1, 2, 3]; //const arr2: number[]
+
+  const roarr: ReadonlyArray<string> = ["str1", "str2", "str3"];
+  // roarr[0] = "test1"; // Index signature in type 'readonly string[]' only permits reading.ts(2542)
+
+  interface IState {
+    data: {};
+    tag: string;
+  }
+
+  function action(state: IState) {
+    state.data = "abc"; // ok
+  }
+
+  function actionReadonly(state: Readonly<IState>) {
+    //state.data = "abc"; // Cannot assign to 'data' because it is a read-only property.ts(2540)
+  }
+}
