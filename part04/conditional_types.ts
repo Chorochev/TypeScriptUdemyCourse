ef21b736e01e1e85ae9a4945eb7a0d6debbdc35e
chorochev <chorochev@ya.ru>
2023-07-14 13:32:05 +0600

    Conditional types

---
+++ part04/conditional_types.ts
@@ -0,0 +1,35 @@
+// Conditional types and infer
+{
+  const conditionalExample = 1 == 1 ? true : false;
+  // SomeType extends OtherType ? TrueType : FalseType;
+  type Example1 = "string" extends "Hello" ? string : number; // type Example = number
+  type Example2 = "string" extends "string" ? string : number; // type Example2 = string
+
+  const strType1: string = "string";
+  const strType2: string = "Hello";
+  // type Example3 = strType1 extends "Hello" ? string : number; // Error: 'strType1' refers to a value, but is being used as a type here. Did you mean 'typeof strType1'?ts(2749)
+  // type Example4 = strType1 extends strType2 ? string : number; // Error
+}
+{
+  interface IDataFromUser {
+    weight: string;
+  }
+
+  interface IDataFromBase {
+    calories: number;
+  }
+
+  type TFromUserOrFromBase<T extends string | number> = T extends string
+    ? IDataFromUser
+    : IDataFromBase;
+
+  const data1: TFromUserOrFromBase<string> = {
+    weight: "Admin",
+  };
+
+  const data2: TFromUserOrFromBase<number> = {
+    calories: 200,
+  };
+
+  console.log(`${data1.weight} ${data2.calories}`);
+}
