ce548642702759c5e628945af167bc8d0afaa8a6
chorochev <chorochev@ya.ru>
2023-07-14 13:36:36 +0600

    Conditional types

--- part04/conditional_types.ts
+++ part04/conditional_types.ts
@@ -10,6 +10,7 @@
   // type Example3 = strType1 extends "Hello" ? string : number; // Error: 'strType1' refers to a value, but is being used as a type here. Did you mean 'typeof strType1'?ts(2749)
   // type Example4 = strType1 extends strType2 ? string : number; // Error
 }
+
 {
   interface IDataFromUser {
     weight: string;
@@ -32,4 +33,18 @@
   };
 
   console.log(`${data1.weight} ${data2.calories}`);
+}
+
+{
+  interface User<T extends "created" | Date> {
+    created: T extends "created" ? "created" : Date;
+  }
+
+  const user1: User<"created"> = {
+    created: "created",
+  };
+
+  const user2: User<Date> = {
+    created: new Date(),
+  };
 }
