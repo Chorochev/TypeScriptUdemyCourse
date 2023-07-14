fa45b35c14ab9b42d4ceb27cbccae7833297f3a7
chorochev <chorochev@ya.ru>
2023-07-14 11:34:59 +0600

    task 8

--- homework/task08/answer01.ts
+++ homework/task08/answer01.ts
@@ -39,12 +39,35 @@
     initialDate: string;
   }
 
-  function filterPhonesByDate<T>(
-    phones: IMobilePhone<T>[],
-    key: string,
+  function filterPhonesByDate<T, K extends keyof T>(
+    phones: T[],
+    key: K,
     initial: string
   ): IPhonesManufacturedAfterDate<T>[] {
     const result: IPhonesManufacturedAfterDate<T>[] = [];
+    // type TPhoneKeys = keyof IMobilePhone<T>;
+    // const keyType: TPhoneKeys = keyof typeof key;
+    // phones.forEach((element) => {
+    //   console.log(element[key]);
+    // });
+
+    phones.forEach((e) => {
+      // if(e[key] == "2022-09-01T00:00:00.000Z"){
+      //     result.push({
+      //         company: e.company,
+      //         number: 4552833,
+      //         size: "5.7",
+      //         companyPartner: "no data",
+      //         manufactured: new Date("2022-05-24T12:00:00"),
+      //         initialDate: "string"
+      //     })
+      // }
+      const dt = e[key];
+      type tdt = typeof dt;
+      console.log(`${e}  ${e[key]}  ${typeof key} ${dt}`);
+    });
+
+    // const result = phones.filter((e) => e[key] == "2022-09-01T00:00:00.000Z");
 
     return result;
   }
