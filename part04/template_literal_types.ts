69bb326d1a55e009eb925770295b3e6426f411ea
chorochev <chorochev@ya.ru>
2023-07-17 10:55:33 +0600

    Template literal types

--- part04/template_literal_types.ts
+++ part04/template_literal_types.ts
@@ -55,3 +55,11 @@
   //     "customKz}": string;
   //   }
 }
+
+{
+  type EmailLocaleIDs = "welcome_email" | "email_heading";
+  type FooterLocaleIDs = "footer_title" | "footer_sendoff";
+
+  type AllLocaleIDs = `${EmailLocaleIDs | FooterLocaleIDs}_id`;
+  // type AllLocaleIDs = "welcome_email_id" | "email_heading_id" | "footer_title_id" | "footer_sendoff_id"
+}
