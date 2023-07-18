77664302d3274ab78f3d310ab886e6be7aa450d3
chorochev <chorochev@ya.ru>
2023-07-18 13:21:52 +0600

    form result  1

--- homework/task09/form_result1.ts
+++ homework/task09/form_result1.ts
@@ -18,3 +18,26 @@
 
   console.log(validationData);
 }
+
+{
+  interface IForm {
+    login: string;
+    password: string;
+    surname: string;
+  }
+
+  interface IValidationData {
+    isValid: boolean;
+    errorMsg?: string;
+  }
+
+  type TValidationData = Record<keyof IForm, IValidationData>;
+
+  const validationData: TValidationData = {
+    login: { isValid: false, errorMsg: "At least 3 characters" },
+    password: { isValid: true },
+    surname: { isValid: true },
+  };
+
+  console.log(validationData);
+}
