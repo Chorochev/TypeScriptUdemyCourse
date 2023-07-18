c3040fa09f74da44fa825d03d4580dfbc577abc6
chorochev <chorochev@ya.ru>
2023-07-18 20:33:25 +0600

    Awaited

---
+++ part04/awaited.ts
@@ -0,0 +1,28 @@
+// Awaited
+{
+  // type FromPromise1 = Promise<number>
+  type FromPromise1 = Promise<number>;
+  // type FromPromise2 = number
+  type FromPromise2 = Awaited<Promise<number>>;
+  // type FromPromise3 = number
+  type FromPromise3 = Awaited<Promise<Promise<number>>>;
+}
+
+{
+  interface IUser {
+    name: string;
+  }
+
+  async function fetchUsers(): Promise<IUser[]> {
+    const users: IUser[] = [{ name: "Alex" }, { name: "John" }];
+    return users;
+  }
+  // const users: Promise<IUser[]>
+  const users = fetchUsers();
+  // type FetchUsersReturnType = IUser[]
+  type FetchUsersReturnType = Awaited<ReturnType<typeof fetchUsers>>;
+  // type UnwrappedPromise<T> = T extends Promise<infer Return> ? Return : T
+  type UnwrappedPromise<T> = T extends Promise<infer Return> ? Return : T;
+  // type FetchDataReturnType = IUser[]
+  type FetchDataReturnType = UnwrappedPromise<ReturnType<typeof fetchUsers>>;
+}
