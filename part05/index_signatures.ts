ef3ca3b0140c2e31e901a6458a23c499a133957c
chorochev <chorochev@ya.ru>
2023-07-19 16:06:47 +0600

    index signatures

---
+++ part05/index_signatures.ts
@@ -0,0 +1,44 @@
+// index signatures
+{
+  class Styles {
+    [s: string]: string;
+  }
+
+  const style = new Styles();
+  style.color = "red";
+  style.font = "Console";
+}
+
+{
+  class Styles {
+    [s: string]: string | ((s: string) => boolean);
+
+    isProperty(name: string): boolean {
+      return name in this;
+    }
+  }
+
+  const style = new Styles();
+  style.color = "red";
+  style.font = "Console";
+
+  console.log(style.isProperty("test")); // false
+  console.log(style.isProperty("color")); // true
+}
+
+{
+  class MyClass {
+    [s: string]: boolean | ((s: string) => boolean);
+
+    check(s: string) {
+      return this[s] as boolean;
+    }
+  }
+
+  const c = new MyClass();
+  c.test1 = true;
+  c.test2 = false;
+
+  console.log(c.check("test1")); // true
+  console.log(c.check("test2")); // false
+}
